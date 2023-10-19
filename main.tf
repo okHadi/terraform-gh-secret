952f485178c13d64d009b6d2e4d7719be0932f4d
okHadi <khan.hadi2951@gmail.com>
2023-10-19 22:06:59 +0500

    test

--- main.tf
+++ main.tf
@@ -5,28 +5,12 @@ terraform {
     }
   }
 }
-variable "datadog_api_key" {
-    type = string
-    description = "Datadog API key"
-    default = "DEFAULTVALUE"
-}
+variable "datadog_api_key" {}
 
-variable "datadog_app_key" {
-        type = string
-    description = "Datadog APP key"
-    default = "APP KEY DATADOG"
-}
 
 # Configure the Datadog provider
 provider "datadog" {
   api_key = var.datadog_api_key
-  app_key = var.datadog_app_key
+  #app_key = var.datadog_app_key
 }
 
-output "api_key" {
-  value = var.datadog_app_key
-}
-
-output "app_key" {
-  value = var.datadog_app_key
-}
