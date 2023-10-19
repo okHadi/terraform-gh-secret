5db13cc65e9c95d4d4d2ac65d9a0d92902b1e24b
okHadi <khan.hadi2951@gmail.com>
2023-10-19 23:18:08 +0500

    docker file added

--- main.tf
+++ main.tf
@@ -1,10 +1,4 @@
-terraform {
-  required_providers {
-    datadog = {
-      source = "DataDog/datadog"
-    }
-  }
-}
+
 variable "datadog_api_key" {}
 variable "datadog_app_key" {}
 
@@ -14,15 +8,39 @@ provider "datadog" {
   app_key = var.datadog_app_key
 }
 
-resource "datadog_monitor" "example_monitor" {
-  name               = "Example Monitor"
-  type               = "metric alert"
-  query              = "avg(last_1h):sum:system.cpu.idle{*} < 75"
-  message            = "CPU idle is below 75% for the last hour"
-  escalation_message = "Alerting for an extended period"
+resource "datadog_monitor" "webapp_cpu_monitor" {
+
+  name        = "Azure Web App CPU Usage"
+
+  type        = "metric alert"
+
+  message     = "The CPU usage of the Azure Web App has exceeded the threshold."
+
+  query       = "max:azure.webapp.cpu{*} by {app_name} > 80"
+
+  monitor_thresholds {
+
+    critical = 80
+
+  }
+
+  notify_no_data = true
+
+  no_data_timeframe {
+
+    minutes = 15
+
+  }
+
+  tags = [
+
+    "environment:production",
+
+    "application:azure_webapp"
+
+  ]
 
 }
-
 
 output "datadog_api_key" {
   value = "${var.datadog_api_key}"
